66119704bcb2372edec16dab13445ac9dcc1f859
NatiDeme <demelashnatnael@hotmail.com>
2023-03-13 12:09:33 +0300

    Add vue2 project initial file

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="QR component" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
